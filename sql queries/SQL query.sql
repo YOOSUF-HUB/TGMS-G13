3999b1f8181591df99b3c55d263c190d9e052abc
saabikweb <mohammedshaabik@gmail.com>
2024-09-19 21:50:11 +0530

    SQL Query files

---
+++ sql queries/SQL query.sql
@@ -0,0 +1,11 @@
+CREATE TABLE customer (
+    customer_id_num INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstname varchar(255),
+    lastname varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    address varchar(255),
+    phone_no varchar(255),
+    dob DATE,
+    date_created DATE DEFAULT CURRENT_DATE
+);
