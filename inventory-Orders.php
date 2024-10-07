352a508f79a60d4a3fc86eb4702ca328f880ea55
saabikweb <mohammedshaabik@gmail.com>
2024-10-07 14:34:40 +0530

    title

--- inventory-Orders.php
+++ inventory-Orders.php
@@ -83,7 +83,7 @@ if($_SESSION['staff_role']!=='Inventory'){ //condition make sure admin user redi
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inventory Dashboard</title>
+    <title>Inventory Dashboard - Orders</title>
     <link rel="stylesheet" href="./styles/inventory.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- social media icons -->
 </head>
